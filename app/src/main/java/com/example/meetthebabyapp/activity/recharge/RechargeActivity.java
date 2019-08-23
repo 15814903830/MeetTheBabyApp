4348cef7992262d06a2ed8d395508f417f8c7547
csp <1580465173@qq.com>
2019-08-23 14:10:03 +0800

    创建 5-2-1充值

---
+++ app/src/main/java/com/example/meetthebabyapp/activity/recharge/RechargeActivity.java
@@ -0,0 +1,18 @@
+package com.example.meetthebabyapp.activity.recharge;
+
+import android.os.Bundle;
+
+import com.example.meetthebabyapp.R;
+import com.example.meetthebabyapp.base.BaseActivity;
+
+/**
+ * 5-2-1充值
+ */
+public class RechargeActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recharge);
+    }
+}
